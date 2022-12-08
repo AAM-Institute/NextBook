4f1c22b2c515aeed50ee2ab4187f7459fb99e02b
Lacy Morrow <me@lacymorrow.com>
2022-12-08 15:34:48 -0500

    adding auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,34 +1,14 @@
 import NextAuth from 'next-auth';
-import EmailProvider from "next-auth/providers/email"
 import GithubProvider from 'next-auth/providers/github';
-// import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-
-// import clientPromise from '../../../utils/mongodb'
-// import AppleProvider from 'next-auth/providers/apple';
-// import FacebookProvider from 'next-auth/providers/facebook';
-// import GoogleProvider from 'next-auth/providers/google';
-// import TwitterProvider from 'next-auth/providers/twitter';
+// import EmailProvider from "next-auth/providers/email"
 
 export const authOptions = {
     secret: process.env.NEXTAUTH_SECRET || undefined,
-    // adapter: MongoDBAdapter(clientPromise),
-    // theme: {
-    //     colorScheme: 'auto',
-    //     brandColor: '#f00',
-    //     logo: '',
-    // },
-    // Configure one or more authentication providers
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-        // EmailProvider({
-        //   server: process.env.EMAIL_SERVER,
-        //   from: process.env.EMAIL_FROM,
-        //   // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
-        // }),
-    // ...add more providers here
     ],
     callbacks: {
         async signIn({ user, account, profile, email, credentials }) {
