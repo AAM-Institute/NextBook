9b4472a959bebfa03665e11ecbb6593044ea3b03
Lacy Morrow <me@lacymorrow.com>
2022-12-11 19:12:18 -0500

    fix embed

--- components/component-mapper.jsx
+++ components/component-mapper.jsx
@@ -14,12 +14,59 @@ import { YouTube } from "mdx-embed/dist/components/youtube";
 
 import Counter from './example-counter'
 import GithubTestRunner from './github-test-runner'
-import components from './mdx-components'
+import {
+  Accordion,
+  Blockquote,
+  Code,
+  CustomImage,
+  CustomLink,
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Hr,
+  Ol,
+  P,
+  Tab,
+  Table,
+  Tabs,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Ul
+} from './mdx-components'
 
 export const componentMap = {
-  ...components,
   Counter,
   GithubTestRunner,
+
+  Accordion,
+  Tab,
+  Tabs,
+  table: Table,
+  thead: Thead,
+  tbody: Tbody,
+  tr: Tr,
+  td: Td,
+  th: Th,
+  a: CustomLink,
+  img: CustomImage,
+  blockquote: Blockquote,
+  code: Code,
+  h1: H1,
+  h2: H2,
+  h3: H3,
+  h4: H4,
+  h5: H5,
+  h6: H6,
+  ol: Ol,
+  ul: Ul,
+  hr: Hr,
+  p: P,
   
   // mdx-embed
   Airtable,
