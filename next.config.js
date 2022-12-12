bc7c48a53f3a09fe75355b7e24234f4b930d0e9d
Lacy Morrow <me@lacymorrow.com>
2022-12-11 22:49:22 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,  
+  // Adding policies:
+  async headers() {
+    return [
+        {
+          source: '/(.*)',
+          headers: [
+            {
+              key: 'X-Frame-Options',
+              value: 'DENY',
+            },
+            {
+              key: 'Content-Security-Policy',
+              value:
+                "default-src 'self'; image-src 'https://unsplash.com'; script-src 'self';font-src 'self' 'https://fonts.googleapis.com'",
+            },
+            {
+              key: 'X-Content-Type-Options',
+              value: 'nosniff',
+            },
+            {
+              key: 'Permissions-Policy',
+              value: "camera=(); battery=(self); geolocation=(); microphone=(self); payment=(); usb=() gyroscope=(); magnetometer=(); accelerometer=(); ",
+            },
+            {
+              key: 'Referrer-Policy',
+              value: 'origin-when-cross-origin',
+            },
+          ],
+        },
+      ];
+  },   
   async rewrites() {
     return [
       {
