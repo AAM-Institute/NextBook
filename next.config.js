50652c0954028c831d66d5e79338a28a674a82f2
Lacy Morrow <me@lacymorrow.com>
2022-12-12 15:07:45 -0500

    test

--- next.config.js
+++ next.config.js
@@ -11,19 +11,19 @@ module.exports = {
               key: 'X-Frame-Options',
               value: 'DENY',
             },
-            {
-              key: 'Content-Security-Policy',
-              value:
-                "default-src * ; image-src 'https://unsplash.com'; script-src * ;font-src 'self' 'https://fonts.googleapis.com'",
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: "camera=(); battery=(self); geolocation=(); microphone=(self); payment=(); usb=() gyroscope=(); magnetometer=(); accelerometer=(); ",
-            },
+            // {
+            //   key: 'Content-Security-Policy',
+            //   value:
+            //     "default-src * ; image-src 'https://unsplash.com'; script-src * ;font-src 'self' 'https://fonts.googleapis.com'",
+            // },
+            // {
+            //   key: 'X-Content-Type-Options',
+            //   value: 'nosniff',
+            // },
+            // {
+            //   key: 'Permissions-Policy',
+            //   value: "camera=(); battery=(self); geolocation=(); microphone=(self); payment=(); usb=() gyroscope=(); magnetometer=(); accelerometer=(); ",
+            // },
             // {
             //   key: 'Referrer-Policy',
             //   value: 'origin-when-cross-origin',
