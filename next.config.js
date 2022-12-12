7c130065f4ec1d16e29723b7f8651c6ec6e4d406
Lacy Morrow <me@lacymorrow.com>
2022-12-12 14:43:02 -0500

    headers

--- next.config.js
+++ next.config.js
@@ -2,36 +2,36 @@ module.exports = {
   // reactStrictMode: true,
   // swcMinify: true,  
   // Adding policies:
-  async headers() {
-    return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'X-Frame-Options',
-              value: 'DENY',
-            },
-            {
-              key: 'Content-Security-Policy',
-              value:
-                "default-src * 'self'; image-src 'https://unsplash.com'; script-src * 'self' 'https://vitals.vercel-insights.com';font-src 'self' 'https://fonts.googleapis.com'",
-            },
-            {
-              key: 'X-Content-Type-Options',
-              value: 'nosniff',
-            },
-            {
-              key: 'Permissions-Policy',
-              value: "camera=(); battery=(self); geolocation=(); microphone=(self); payment=(); usb=() gyroscope=(); magnetometer=(); accelerometer=(); ",
-            },
-            // {
-            //   key: 'Referrer-Policy',
-            //   value: 'origin-when-cross-origin',
-            // },
-          ],
-        },
-      ];
-  },   
+  // async headers() {
+  //   return [
+  //       {
+  //         source: '/(.*)',
+  //         headers: [
+  //           {
+  //             key: 'X-Frame-Options',
+  //             value: 'DENY',
+  //           },
+  //           {
+  //             key: 'Content-Security-Policy',
+  //             value:
+  //               "default-src * 'self'; image-src 'https://unsplash.com'; script-src * 'self' 'https://vitals.vercel-insights.com';font-src 'self' 'https://fonts.googleapis.com'",
+  //           },
+  //           {
+  //             key: 'X-Content-Type-Options',
+  //             value: 'nosniff',
+  //           },
+  //           {
+  //             key: 'Permissions-Policy',
+  //             value: "camera=(); battery=(self); geolocation=(); microphone=(self); payment=(); usb=() gyroscope=(); magnetometer=(); accelerometer=(); ",
+  //           },
+  //           // {
+  //           //   key: 'Referrer-Policy',
+  //           //   value: 'origin-when-cross-origin',
+  //           // },
+  //         ],
+  //       },
+  //     ];
+  // },   
   async rewrites() {
     return [
       {
