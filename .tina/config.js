ced15f36f6665f6e0f5b109daef5e52cb331614c
Lacy Morrow <me@lacymorrow.com>
2023-01-25 13:22:58 -0500

    remove public .env

--- .tina/config.js
+++ .tina/config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tinacms'
 
   // Your hosting provider likely exposes this as an environment variable
-  const branch = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||  process.env.HEAD || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || 'main'
+  const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || process.env.HEAD || 'main'
   
   export default defineConfig({
     // Get this from tina.io
