3275c0af78da22d1e636b4cd909497e16d744820
Lacy Morrow <me@lacymorrow.com>
2024-07-27 00:08:04 -0400

    fix

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,11 @@
 import config from '@/config/config.json';
-import Document, { Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang={config.locale}>
+        <Head />
         <body className='dark:bg-gray-800 text-gray-800 dark:text-gray-100'>
           <Main />
           <NextScript />
